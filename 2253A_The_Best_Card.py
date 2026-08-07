7bdb7c2506a7be71b0d597015edc5202fda6a524
Tanishq Das <tanishqdas2005@gmail.com>
2026-08-07 20:14:39 +0530

    Sync Codeforces: 2253A - The Best Card [Python 3]

---
+++ 2253A_The_Best_Card.py
@@ -0,0 +1,13 @@
+def p(n):
+ if n<2:return 0
+ if n==2:return 1
+ if n%2==0:return 0
+ i=3
+ while i*i<=n:
+  if n%i==0:return 0
+  i+=2
+ return 1
+t=int(input())
+for _ in range(t):
+ n=int(input())
+ print("YES"if p(n+1)else"NO")
